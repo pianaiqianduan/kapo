2c2267036ffee8f6bdea8b2aeb5d6c04b39de545
pianaiqianduan <aiqianduan@gmail.com>
2019-01-11 10:09:42 +0800

    给选择门店添加加载动画

--- src/pages/selectStore/selectStore.vue
+++ src/pages/selectStore/selectStore.vue
@@ -94,7 +94,7 @@ export default {
                 this.storeName = val.title 
                 setTimeout(()=>{
                     this.$vux.loading.hide()
-                },500)
+                },1000)
             }
                          
         },
